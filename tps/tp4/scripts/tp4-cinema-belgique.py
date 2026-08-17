9fb90f587ca429262c21a988a85a5cb0febc9a80
Rufin Hounkpe <rhounkpe@gmail.com>
2026-08-17 07:08:44 +0200

    Query CAMille for cinema

--- tps/tp4/scripts/tp4-cinema-belgique.py
+++ tps/tp4/scripts/tp4-cinema-belgique.py
@@ -1,14 +1,22 @@
 import os
+
 import requests
 
+
+query = "cinema"
+
 response = requests.get(
-    "https://camille.ulb.be/?page=about",
+    "https://camille.ulb.be/",
+    params={
+        "query": query,
+    },
     headers={
         "Cookie": os.environ["CAMILLE_COOKIE"]
     },
-    allow_redirects=False,
     timeout=30,
 )
 
+print("Recherche :", query)
 print("Status :", response.status_code)
-print("Location :", response.headers.get("Location"))
+print("URL :", response.url)
+print("Taille de la réponse :", len(response.text), "caractères")

75cfd5570404d1323fd34d647e0d45ef6fbabeca
Rufin Hounkpe <rhounkpe@gmail.com>
2026-08-17 07:12:30 +0200

    Filter CAMille search results by French language

--- tps/tp4/scripts/tp4-cinema-belgique.py
+++ tps/tp4/scripts/tp4-cinema-belgique.py
@@ -5,11 +5,13 @@ from bs4 import BeautifulSoup
 
 
 query = "cinema"
+language = "fr-BE"
 
 response = requests.get(
     "https://camille.ulb.be/",
     params={
         "query": query,
+        "language": language,
     },
     headers={
         "Cookie": os.environ["CAMILLE_COOKIE"]
@@ -22,6 +24,7 @@ response.raise_for_status()
 soup = BeautifulSoup(response.text, "html.parser")
 
 print("Recherche :", query)
+print("Langue :", language)
 
 for text in soup.stripped_strings:
     if "résultats (" in text.lower():

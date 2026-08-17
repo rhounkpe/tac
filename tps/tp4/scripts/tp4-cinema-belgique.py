9540390b0f46bde75ed10c399dda9b92d811ca23
Rufin Hounkpe <rhounkpe@gmail.com>
2026-08-17 07:20:59 +0200

    Filter CAMille search by selected publications

--- tps/tp4/scripts/tp4-cinema-belgique.py
+++ tps/tp4/scripts/tp4-cinema-belgique.py
@@ -9,14 +9,30 @@ language = "fr-BE"
 year_from = 1885
 year_to = 1960
 
+papers = {
+    "JB427": "La Libre Belgique",
+    "JB555": "L'Indépendance Belge",
+    "JB567": "Journal de Bruxelles",
+    "JB837": "Le Peuple",
+    "JB1051": "Le Drapeau rouge",
+    "BU1": "Annuaire Officiel de la presse belge",
+    "JO3": "Journalistes 1979--2004",
+}
+
+params = [
+    ("query", query),
+    ("sortcrit", "relevance"),
+    ("year_from", year_from),
+    ("year_to", year_to),
+    ("language", language),
+]
+
+for paper_id in papers:
+    params.append(("paper", paper_id))
+
 response = requests.get(
     "https://camille.ulb.be/",
-    params={
-        "query": query,
-        "language": language,
-        "year_from": year_from,
-        "year_to": year_to,
-    },
+    params=params,
     headers={
         "Cookie": os.environ["CAMILLE_COOKIE"]
     },
@@ -24,11 +40,18 @@ response = requests.get(
 )
 
 response.raise_for_status()
+print("URL :", response.url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
 print("Recherche :", query)
 print("Langue :", language)
+print("Période :", year_from, "-", year_to)
+
+print("Titres sélectionnés :")
+for title in papers.values():
+    print("-", title)
+
 
 for text in soup.stripped_strings:
     if "résultats (" in text.lower():
